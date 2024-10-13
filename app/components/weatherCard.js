828ce73cdb8fee7afa60fbef0ad13f595aaf918b
Dcb9250 <123490986+Dcb9250@users.noreply.github.com>
2024-10-13 18:04:23 -0400

    Oct 13th

--- app/components/weatherCard.js
+++ app/components/weatherCard.js
@@ -1,13 +1,29 @@
-export default function WeatherCard({city, currentTemp, highTemp, lowTemp, cloudiness, humidity, windSpeed}) {
-    return (
-        <div>
-            <h2>{city}</h2>
-            <p>Current Temperature: {currentTemp}</p>
-            <p>High Temperature: {highTemp}</p>
-            <p>Low Temperature: {lowTemp}</p>
-            <p>Cloudiness: {cloudiness}</p>
-            <p>Humidity: {humidity}</p>
-            <p>Wind Speed: {windSpeed}</p>
-        </div>
-    )
+import WeatherIcon from "./WeatherIcon"
+
+export default function WeatherCard({
+	city,
+	cloudiness,
+	currentTemp,
+	highTemp,
+	humidity,
+	lowTemp,
+	weatherType,
+	windSpeed,
+}) {
+	console.log({ cloudiness });
+	const cloudinessPercentage = 99 - cloudiness;
+	const background = `linear-gradient(90deg, white, ${cloudinessPercentage}%, grey)`;
+	return (
+		<div style={{ background }}>
+			<h2>{city}</h2>
+			<p>Current Temperature: {currentTemp}</p>
+			<p>High Temperature: {highTemp}</p>
+			<p>Low Temperature: {lowTemp}</p>
+			<p>Cloudiness: {cloudiness}</p>
+			<p>Humidity: {humidity}</p>
+			<p>Wind Speed: {windSpeed}</p>
+			weatherType = {weatherType}
+            <WeatherIcon weatherType= {weatherType}/>
+		</div>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -1,37 +1,31 @@
 import WeatherCard from "./components/WeatherCard";
 import styles from "./page.module.css";
-import Header from "./components/Header"
-
+import Header from "./components/Header";
 
 export default async function Home() {
-  const city = "Boston";
-  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
-  
+	const city = "Boston";
+	const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
 
-  const request = await fetch(queryURL);
-  const weatherData = await request.json();
+	const request = await fetch(queryURL);
+	const weatherData = await request.json();
 
-  console.log(weatherData);
-
-
-
-  return (
-    <div className ={styles.page}>
-      <Header />
-      <main className={styles.main}>
- 
-        <div className={styles.WeatherCardWrapper}>
-          <WeatherCard 
-            city={city} 
-            currentTemp={weatherData.main.temp}
-            highTemp= {weatherData.main.temp_max} 
-            lowTemp={weatherData.main.temp_min}
-            cloudiness={weatherData.clouds.all}
-            humidity={weatherData.main.humidity}
-            windSpeed={weatherData.wind.speed}
-          />
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.page}>
+			<Header />
+			<main className={styles.main}>
+				<div className={styles.weatherCardWrapper}>
+					<WeatherCard
+						city={city}
+						currentTemp={weatherData.main.temp}
+						highTemp={weatherData.main.temp_max}
+						lowTemp={weatherData.main.temp_min}
+						cloudiness={weatherData.clouds.all}
+						humidity={weatherData.main.humidity}
+						windSpeed={weatherData.wind.speed}
+						weatherType={weatherData.weather[0].main}
+					/>
+				</div>
+			</main>
+		</div>
+	);
 }
